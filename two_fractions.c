e350245cace98b2e367877a9555110f27832b5d2
ENG19CS0272 SAGAR M A <eng19cs0272.masagar@gmail.com>
2021-02-11 10:27:51 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,55 +1,45 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
-typedef struct fraction{
-int numerator, denominator;
-}Fractions;
 
-Fractions input(Fractions temp){
-printf("Enter the numerator:");
-scanf("%d",&temp.numerator);
-printf("Enter the denominator:");
-scanf("%d",&temp.denominator);
-return temp;
-};
+struct frac
+{
+ int num ,den;
+}
+frac1,frac2;
 
-void show_output(Fractions temp,Fractions a, Fractions b){
-printf("The sum of the two fraction %d/%d+%d/%d = %d/%d",a.numerator,a.denominator,b.numerator,b.denominator,temp.numerator,temp.denominator);
-};
+int output(int f1num,int f1den,int f2num,int f2den,int a,int b)
+{
+printf("the addition of two fracs when  the first frac is %d%d and second frac is %d%d is:%d%d/n",f1num,f1den,f2num,f2den,a,b);
+}
+int add(int frac1num,int frac1den,int frac2num,int frac2den) 
+{
+int num ,den ;
 
-int gcd(int x,int y){
-    if (x ==0){
-        return y;
-    }
-    return gcd(y%x,x);
-};
+num =((frac1num*frac2den) +( frac2num*frac1den));
+den =(frac1den*frac2den);
 
-Fractions calc_output(Fractions a, Fractions b){
-    Fractions c;
-    int x,y;
-    x = (a.numerator * b.denominator) + (b.numerator * a.denominator);
-    y = (a.denominator * b.denominator);
+output(frac1.num,frac2.den,frac2.num,frac2.den,num,den);
+}
 
-    
-    c.numerator = x /gcd(x,y);
-    c.denominator = y /gcd(x,y);
-    return c;
-};
+int main()
+{
+ printf("the first frac:/n");
+printf("enter value of num:/n");
+scanf("%d",& frac1.num);
+printf("enter value of den:/n");
+scanf("%d",& frac1.den);
 
+printf("the second frac:/n");
+printf("enter value of  num:/n");
+scanf("%d", & frac2.num);
+printf("enter value of den:/n");
+scanf("%d",& frac2.den);
 
-int main(){
-Fractions a,b,c;
-int gcd;
-printf("Enter 1st fraction:\n");
-a = input(a);
+add(frac1.num,frac1.den,frac2.num,frac2.den);
 
-printf("Enter 2nd fraction:\n");
-b = input(b);
-
-c = calc_output(a,b);
+return 0;
+}
 
 
 
-show_output(c,a,b);
-
-
-return 0;}
+      
