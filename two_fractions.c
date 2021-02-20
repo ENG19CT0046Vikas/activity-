6ecf93887669a48146ed7a241ce1b4d27e2e36cc
ENG19CT0046Vikas <75782605+ENG19CT0046Vikas@users.noreply.github.com>
2021-02-20 11:18:17 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -9,7 +9,7 @@ frac1,frac2;
 
 int output(int f1num,int f1den,int f2num,int f2den,int a,int b)
 {
-printf("the addition of two fracs when  the first frac is %d%d and second frac is %d%d is:%d%d/n",f1num,f1den,f2num,f2den,a,b);
+printf("the addition of two fracs when  the first frac is %d/%d and second frac is %d/%d is:%d/%d\n",f1num,f1den,f2num,f2den,a,b);
 }
 int add(int frac1num,int frac1den,int frac2num,int frac2den) 
 {
@@ -32,7 +32,7 @@ scanf("%d",& frac1.den);
 printf("the second frac:\n");
 printf("enter value of  num:\n");
 scanf("%d", & frac2.num);
-printf("enter value of den:\n");
+printf("enter value of den:\n"); 
 scanf("%d",& frac2.den);
 
 add(frac1.num,frac1.den,frac2.num,frac2.den);
